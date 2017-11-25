504383ebbef70fad3689b3faf1a875d37c80fe44
Yoosuf <mayoosuf@gmail.com>
2017-11-25 08:58:37 +0530

    project organisation

---
+++ app/Entities/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Admin extends Model
+{
+    use Notifiable;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+    protected $fillable = [
+      'name', 'email', 'password',
+    ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+    protected $hidden = [
+      'password', 'remember_token',
+    ];
+}
