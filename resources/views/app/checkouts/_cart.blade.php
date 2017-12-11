3cfabb3f68028a33b8ab93bf227f4c0d9ea495ac
mudithalasantha <mudithalasantha@gmail.com>
2017-12-11 17:46:04 +0530

    checkout WIP

--- resources/views/app/checkouts/_cart.blade.php
+++ resources/views/app/checkouts/_cart.blade.php
@@ -5,24 +5,23 @@
         <h1 class="title is-4 is-spaced">Your order details</h1>
         <p class="subtitle is-5"></p>
 
+        <h2 class="is-success">Order Summary</h2>
+        <br>
+        <br>
+        @foreach($grouped as $key => $partner)
 
-        @foreach(Cart::content() as $row)
+            <h3 class="is-success">by {{$key}}</h3>
 
-            <input type="hidden" name="id" id="id" value="{{$row->id}}">
-            <div class="media">
-                <figure class="media-left">
-                    <p class="image is-64x64">
-                        <img src="https://bulma.io/images/placeholders/128x128.png">
-                    </p>
-                </figure>
+            {{$discount_percentage = \App\Entities\Partner::where('name', $key)->first()->preferences['']}}
+            {{$max_discount_amount = \App\Entities\Partner::where('name', $key)->first()}}
+
+            @foreach($partner as $item)
+
+            <div class="media" style="padding-left: 100px;">
                 <div class="media-content">
                     <div class="content">
                         <p>
-                            <strong>{{$row->name}}</strong>
-                            <br>
-                            <small>by {{App\Entities\Product::find($row->id)->first()->partner()->first()->name}}</small>
-                            <br>
-                            <medium>&#2547; {{number_format(((float)$row->price), 2, '.', '')}}</medium>
+                            <strong>{{$item['item']->name}} (&#2547; {{number_format(((float)$item['item']->price), 2, '.', '')}}) ({{$item['item']->qty}} units)</strong>
                         </p>
                     </div>
                 </div>
@@ -30,36 +29,17 @@
                     <tr>
                         <div class="media-right">
                             <td>
-                                <form role="form" method="POST" action="{{ route('cart.remove') }}">
-                                    {{ csrf_field() }}
-                                    <input type="hidden" name="id" id="id" value="{{$row->rowId}}">
-                                    <button type="submit" class="button is-success">-</button>
-                                </form>
-                            </td>
-                            <td>
-                            <span class="tag is-info">
-                            <strong class="is-success">{{$row->qty}}</strong>
-                            </span>
-                            </td>
-                            <td>
-                                <form role="form" method="POST" action="{{ route('cart.add') }}">
-                                    {{ csrf_field() }}
-                                    <input type="hidden" name="id" id="id" value="{{$row->id}}">
-                                    <button type="submit" class="button is-success">+</button>
-                                </form>
+                                <span class="">
+                                    <strong class="is-success">&#2547; {{$item['item']->qty * number_format(((float)$item['item']->price), 2, '.', '')}}</strong>
+                                </span>
                             </td>
                         </div>
                     </tr>
                 </table>
             </div>
-
+            @endforeach
         @endforeach
 
-
-
-
-
     </div>
-
 
 </div>
