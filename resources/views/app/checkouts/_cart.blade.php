295e93b0baf776043e4ed79e2a5f7257bb4e28f5
mudithalasantha <mudithalasantha@gmail.com>
2017-12-12 13:06:03 +0530

    checkout WIP

--- resources/views/app/checkouts/_cart.blade.php
+++ resources/views/app/checkouts/_cart.blade.php
@@ -11,11 +11,14 @@
         @foreach($grouped as $key => $partner)
 
             <h3 class="is-success">by {{$key}}</h3>
+            <?php $partner_total=0 ?>
+            <?php $discount_percentage = \App\Entities\Partner::where('name', $key)->first()['preferences']['discount_percentage'] ?>
+            <?php $max_discount_amount = \App\Entities\Partner::where('name', $key)->first()['preferences']['max_discount_amount'] ?>
 
-            {{$discount_percentage = \App\Entities\Partner::where('name', $key)->first()->preferences['']}}
-            {{$max_discount_amount = \App\Entities\Partner::where('name', $key)->first()}}
 
             @foreach($partner as $item)
+
+                <?php $partner_total=$partner_total + ($item['item']->qty * number_format(((float)$item['item']->price), 2, '.', '')) ?>
 
             <div class="media" style="padding-left: 100px;">
                 <div class="media-content">
@@ -38,6 +41,18 @@
                 </table>
             </div>
             @endforeach
+            <div class="media" style="padding-left: 100px;">
+                <table style="width: 100%;">
+                    <tr>
+                        <td style="text-align: right">
+                                        <span class="">
+                                            <strong class="is-success">&#2547; {{$partner_total}}</strong>
+                                        </span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
         @endforeach
 
     </div>
