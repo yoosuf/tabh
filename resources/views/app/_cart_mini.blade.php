b117be3642a8d558772da4bf74968e8ec4958e89
mudithalasantha <mudithalasantha@gmail.com>
2017-12-15 17:15:44 +0530

    Fixes WIP

--- resources/views/app/_cart_mini.blade.php
+++ resources/views/app/_cart_mini.blade.php
@@ -4,9 +4,11 @@
         <label class="label is-pulled-left">{{Cart::count()}} Items</label>
         <label class="label is-pulled-right">Total : &#2547; {{ Cart::subtotal() }}</label>
 
+        @if(Cart::count() > 0)
         <div class="control">
             <button type="submit" class="button is-success is-medium is-fullwidth">Proceed to Checkout</button>
         </div>
+        @endif
 
     </div>
 </form>
