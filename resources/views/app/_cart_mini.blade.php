244d7ded7e98ba19c90cc9bafb27a021fff635ab
Yoosuf <mayoosuf@gmail.com>
2017-12-20 09:16:57 +0530

    wip

--- resources/views/app/_cart_mini.blade.php
+++ resources/views/app/_cart_mini.blade.php
@@ -1,10 +1,11 @@
 <form role="form" id="cart_mini" method="GET" action="{{ route('cart.show') }}">
 
+
     <div class="field">
-        <label class="label is-pulled-left">{{Cart::count()}} Items</label>
+        <label class="label is-pulled-left">{{ Cart::count() }} Items</label>
         <label class="label is-pulled-right">Total : &#2547; {{ Cart::subtotal() }}</label>
 
-        @if(Cart::count() > 0)
+        @if(count($cart) > 0)
             <div class="control">
                 <button type="submit" class="button is-success is-medium is-fullwidth">Proceed to checkout</button>
             </div>
--- resources/views/app/_item.blade.php
+++ resources/views/app/_item.blade.php
@@ -7,41 +7,41 @@
     <figure class="media-left">
         <p class="image is-64x64">
             @if($product->attachment()->first() != null)
-            <img src="{{url('attachments/' . get_attachment($product->attachment()->first()))}}">
+                <img src="{{url('attachments/' . get_attachment($product->attachment()->first()))}}">
             @else
-            <img src="/img/DefaultImage.png">
+                <img src="{{ asset('/img/DefaultImage.png')  }}">
             @endif
         </p>
     </figure>
     <div class="media-content">
 
-            <p>
-                <strong>{{$product->title}}</strong>
-                <small>{{$product->generic_name}}</small>
-                <br>
-                {{$product->product_type}}
-                @if($product->product_type != "")
+        <p>
+            <strong>{{$product->title}}</strong>
+            <small>{{$product->generic_name}}</small>
+            <br>
+            {{$product->product_type}}
+            @if($product->product_type != "")
                 |
-                @endif
-                {{$product->packsize}}
-                <br>
-                <small>by {{$product->partner()->first()->name}}</small>
+            @endif
+            {{$product->packsize}}
+            <br>
+            <small>by {{$product->partner()->first()->name}}</small>
 
-            </p>
+        </p>
 
 
         <nav class="level">
-          <div class="level-left">
-            <div class="level-item">
-              <medium>&#2547; {{number_format(((float)$product->price), 2, '.', '')}}</medium>
+            <div class="level-left">
+                <div class="level-item">
+                    <medium>&#2547; {{number_format(((float)$product->price), 2, '.', '')}}</medium>
+                </div>
             </div>
-          </div>
-          <div class="level-right">
-            <div class="level-item">
-              <button type="submit" class="button is-info is-outlined item-button">Add to Cart</button>
+            <div class="level-right">
+                <div class="level-item">
+                    <button type="submit" class="button is-info is-outlined item-button">Add to Cart</button>
 
+                </div>
             </div>
-          </div>
         </nav>
 
     </div>
