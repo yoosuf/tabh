f7c634efff52ecdc1f6e1883162a2eba37aae22d
Yoosuf <mayoosuf@gmail.com>
2017-12-20 19:13:58 +0530

    order view updates

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -4,96 +4,133 @@
 
     <div class="columns">
         <div class="column">
-            <p class="title">Orders</p>
+            <p class="title">Order # {{ $order->id }} </p>
         </div>
     </div>
 
 
     <div class="card">
         <div class="card-content">
-            <div class="field is-grouped">
-                <p class="title">Order</p>&nbsp;
-                <button onclick="window.history.go(-1); return false;" class="button is-text">Back</button>
-            </div>
 
-            <div class="field">
-                <label class="label">ID</label>
-                <div class="box">
-                    <label class="label">{{ isset($order->id) ? $order->id : '' }}</label>
-                </div>
-            </div>
 
-            <div class="field">
-                <label class="label">User</label>
-                <div class="box">
-                    <label class="label">{{ isset($order->user()->first()->email) ? $order->user()->first()->email : '' }}</label>
-                </div>
-            </div>
+    <div class="columns">
+        <div class="column">
+            <p class="title">Order Details</p>
 
-            <div class="field">
-                <label class="label">Status</label>
-                <div class="box">
-                    <label class="label">{{ isset($order->status) ? $order->status : '' }}</label>
-                </div>
-            </div>
+            <p>Order ID:  {{ isset($order->id) ? $order->id : '' }}</p>
 
-            <div class="field">
-                <label class="label">Total</label>
-                <div class="box">
-                    <label class="label">{{ isset($order->total_amount) ? $order->total_amount : '' }}</label>
-                </div>
-            </div>
+            <p>Order Date:  {{ isset($order->created_at) ? $order->created_at : '' }}</p>
+            <p>Status: {{ isset($order->status) ? $order->status : '' }} </p>
+        </div>
 
-            <div class="field">
-                <label class="label">Discount</label>
-                <div class="box">
-                    <label class="label">{{ isset($order->total_discount) ? $order->total_discount : '' }}</label>
-                </div>
-            </div>
+        <div class="column">
+            <p class="title">Customer Details</p>
+            <p>{{ isset($order->user()->first()->email) ? $order->user()->first()->email : '' }}</p>
+        </div>
+    </div>
+    <div class="columns">
+        <div class="column">
 
-            <div class="field">
-                <label class="label">Tax</label>
-                <div class="box">
-                    <label class="label">{{ isset($order->tax) ? $order->tax : '' }}</label>
-                </div>
-            </div>
+            <table class="table is-fullwidth is-hoverable is-striped">
+                <thead>
+                    <tr>
+                        <th>
+                            Qty
+                        </th>
+                        <th>
+                            Title
+                        </th>
+                        <th>
+                            Provider
+                        </th>
 
-            <br>
-            <p class="title">Items</p>&nbsp;
+                        <th>
+                            Price
+                        </th>
+                        <th>                            
+                            Amount
+                        </th>
+                    </tr>
+                </thead>
+                
 
-            @foreach($line_items as $line_item)
+                <tfoot>
 
-                <h3 class="is-success">by {{$line_item->partner()->first()->name}}</h3>
+                
 
-                <div class="media" style="padding-left: 100px;">
-                    <div class="media-content">
-                        <div class="content">
-                            <p>
-                                <strong>{{$line_item->product()->first()->title}}
-                                    (&#2547; {{number_format(((float)$line_item->product()->first()->price), 2, '.', '')}}
-                                    ) ({{$line_item->quantity}} units)</strong>
-                            </p>
-                        </div>
-                    </div>
-                    <table>
-                        <tr>
-                            <div class="media-right">
-                                <td>
-                                <span class="">
-                                    <strong class="is-success">&#2547; {{number_format(((float)$line_item->quantity * number_format(((float)$line_item->product()->first()->price), 2, '.', '')), 2, '.', '')}}</strong>
-                                </span>
-                                </td>
-                            </div>
-                        </tr>
-                    </table>
-                </div>
-            @endforeach
-            <br>
-            <div class="field is-grouped">
-                <div class="control">
-                    <button onclick="window.history.go(-1); return false;" class="button is-text">Back</button>
-                </div>
-            </div>
+                    
+                </tfoot>
+
+
+                <tbody>
+                @foreach($line_items as $line_item)
+                    <tr>
+                        <td>
+                            {{$line_item->quantity}}
+                        </td>
+                        <td>
+                            {{ $line_item->product->title }}
+                        </td>
+
+                        <td>
+                        {{ $line_item->partner->name }}
+                        </td>
+                        <td>
+                            &#2547; {{number_format(((float)$line_item->product()->first()->price), 2, '.', '')}}
+                        </td>
+                        <td>                            
+                            &#2547; {{number_format(((float)$line_item->quantity * number_format(((float)$line_item->product()->first()->price), 2, '.', '')), 2, '.', '')}}
+                        </td>
+                    </tr>
+                @endforeach
+
+                    <tr>
+                        <th colspan="3"></th>
+                        <th>                            
+                            Discount
+                        </th>  
+                        <th>                            
+                            &#2547; {{ isset($order->total_discount) ? $order->total_discount : '' }}
+                        </th>
+                    </tr>
+
+
+
+                    <tr>
+                        <th colspan="3"></th>
+                        <th>                            
+                            Tax
+                        </th>  
+                        <th>                            
+                            &#2547; {{ isset($order->tax) ? $order->tax : '' }}
+                        </th>
+                    </tr>
+
+
+
+
+                    <tr>
+                        <th colspan="3"></th>
+                        <th>                            
+                            Total
+                        </th>  
+                        <th>                            
+                            &#2547; {{ isset($order->total_amount) ? $order->total_amount : '' }}
+                        </th>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+       
+
+            
+
+            
+        
+        
 
         </div>
     </div>
