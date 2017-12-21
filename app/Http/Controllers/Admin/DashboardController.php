5f6f49acb220f773e02ebba57743266c423bc18f
Yoosuf <mayoosuf@gmail.com>
2017-12-21 17:55:03 +0530

    wip

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,23 +2,58 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Entities\Order;
+use App\Entities\Partner;
+use App\Entities\Product;
+use App\Entities\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
 
-    public function __construct()
+    protected $order;
+
+    protected $customer;
+
+    protected $partner;
+
+    protected $product;
+
+
+    public function __construct(Order $order, User $customer, Partner $partner, Product $product)
     {
-         $this->middleware('admin');
+        $this->middleware('admin');
+
+        $this->order = $order;
+        $this->customer = $customer;
+        $this->partner = $partner;
+        $this->product = $product;
+
     }
 
 
     public function index(Request $request)
     {
 
+        $data = [
+            'order_total_count' => $this->order->count(),
+            'order_completed_count' => $this->order->where('status', 'Completed')->count(),
 
-        
+            'customer_count' => $this->customer->count(),
+            'customer_active_count' => $this->customer->where('banned', true)->count(),
+            'customer_de_active_count' => $this->customer->where('banned', false)->count(),
+
+            'product_count' => $this->product->count(),
+            'product_active_count' => $this->product->where('published', true)->count(),
+            'product_de_active_count' => $this->product->where('published', false)->count(),
+
+            'partner_count' => $this->partner->count(),
+            'partner_active_count' => $this->partner->where('is_active', true)->count(),
+            'partner_de_active_count' => $this->partner->where('is_active', false)->count(),
+
+
+        ];
 
         return view('admin.dashboard', get_defined_vars());
 
