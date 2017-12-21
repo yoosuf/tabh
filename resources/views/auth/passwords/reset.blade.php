368108949ad84ae2a20ef541ef4c8c4cb274ab8a
Yoosuf <mayoosuf@gmail.com>
2017-12-21 20:07:36 +0530

    wip

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -20,7 +20,7 @@
                             <input type="hidden" name="token" value="{{ $token }}">
 
                             <div class="field{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="label">{{ trans('quicksilver.email')}}</label>
+                                <label for="email" >{{ trans('quicksilver.email')}}</label>
 
                                 <input id="email" type="email" class="input" name="email"
                                        value="{{ $email or old('email') }}" required autofocus>
@@ -34,7 +34,7 @@
                             </div>
 
                             <div class="field{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="label">{{ trans('quicksilver.password')}}</label>
+                                <label for="password" >{{ trans('quicksilver.password')}}</label>
 
 
                                 <input id="password" type="password" class="input" name="password" required>
@@ -49,7 +49,7 @@
 
                             <div class="field{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                                 <label for="password-confirm"
-                                       class="label">{{ trans('quicksilver.password_confirm')}}</label>
+                                       >{{ trans('quicksilver.password_confirm')}}</label>
 
                                 <input id="password-confirm" type="password" class="input" name="password_confirmation"
                                        required>
